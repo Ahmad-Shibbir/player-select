2e7392dfd17e429b985ba0e0cabe19c449ce15ac
Ahmad-Shibbir <ahmadshibbir.iium@gmail.com>
2022-08-22 03:40:52 +0800

    added function to get elements value

--- JS/budget.js
+++ JS/budget.js
@@ -1,1 +1,16 @@
-console.log("inside budget.js file")
+
+function getElementsValues(elementValue){
+const inputField = document.getElementById(elementValue);
+const elementValueInString = inputField.value;
+const elementValueInFloat = parseFloat(elementValueInString);
+return elementValueInFloat;
+
+}
+
+
+
+document.getElementById('calculate-total-btn').addEventListener('click',function(){
+    
+    const TotalCOst = getElementsValues('per-palyer-cost');
+    console.log(TotalCOst)
+})

8d3a617e9efda3646849c14c15c828af299b4947
Ahmad-Shibbir <ahmadshibbir.iium@gmail.com>
2022-08-22 04:28:58 +0800

    set element values function added

--- JS/budget.js
+++ JS/budget.js
@@ -1,16 +1,44 @@
 
-function getElementsValues(elementValue){
-const inputField = document.getElementById(elementValue);
-const elementValueInString = inputField.value;
+function getElementsValues(elementValue, isItInInputField){
+let inputField = document.getElementById(elementValue);
+let elementValueInString;
+
+if(isItInInputField == true){
+    elementValueInString = inputField.value;
+}
+else if(isItInInputField == false ){
+    elementValueInString = inputField.innerText;
+}
+
 const elementValueInFloat = parseFloat(elementValueInString);
+
 return elementValueInFloat;
 
 }
 
+function setElementsvalues(elementsValueHolder, elementsValue){
+    const textHolder = document.getElementById(elementsValueHolder);
+    textHolder.innerText = elementsValue;
+}
 
 
-document.getElementById('calculate-total-btn').addEventListener('click',function(){
+
+document.getElementById('calculate-player-expence-btn').addEventListener('click',function(){
     
-    const TotalCOst = getElementsValues('per-palyer-cost');
-    console.log(TotalCOst)
+    const getPlayerExpence = getElementsValues('per-palyer-cost',true);
+    setElementsvalues('player-expence-holder',getPlayerExpence)
+    
+
+
+   
+})
+document.getElementById('calculate-total-btn').addEventListener('click',function(){
+     const managerExpence = getElementsValues('manager-expence',true);
+     const coachExpence = getElementsValues('coach-expence',true);
+    const playerExpence = getElementsValues('player-expence-holder',false);
+
+     totalExpence = managerExpence+ coachExpence+playerExpence;
+     setElementsvalues('total-expences',totalExpence)
+    
+    
 })
